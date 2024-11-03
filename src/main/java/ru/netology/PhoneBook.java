c28c7e46c00960c199e9050332f632c1ee776bff
Grigoriy Dyatlov <holy.crapicl@gmail.com>
2024-11-03 20:18:38 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,16 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> phoneBook = new HashMap<>();
+
     public int add (String name, String number){
-        return 0;
+        if (phoneBook.get(name) != null) {
+            IllegalStateException ex = new IllegalStateException("This number already added.");
+            throw ex;
+        } else phoneBook.putIfAbsent(name, number);
+        return phoneBook.size();
     }
 }
