7b36999df997ecb2589fa44c96471eaead59a4c2
Grigoriy Dyatlov <holy.crapicl@gmail.com>
2024-11-03 19:53:25 +0300

    Stub and tests for add.

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class PhoneBookTest {
+    private PhoneBook phoneBook = null;
+
+    @BeforeEach
+            public void newPhoneBook() {
+        this.phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void testAdd() {
+        int result = phoneBook.add("Kolya", "+7-913-913-91-91");
+
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    public void testExpectedException() throws IllegalStateException {
+        int result = phoneBook.add("Kolya", "+7-913-913-91-91");
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () ->
+        phoneBook.add("Kolya", "+7-913-913-91-91"));
+
+        assertEquals("This number already added.", exception.getMessage());
+    }
 }
